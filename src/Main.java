753893c8419a0dc5cf68659fc9fce3ca5c60622a
Dmitriy <dmitriyshulha@com>
2022-02-20 21:02:22 +0300

    Add divide in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
+    static int a = 4;
+    static int b = 2;
 
     public static void main(String[] args) {
         System.out.println("My git app.");
+
+        int d = divide(a,b);
+        System.out.println(d);
+    }
+
+    private static int divide(int a, int b){
+        return a / b;
     }
 }
